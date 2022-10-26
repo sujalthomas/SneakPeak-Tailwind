bde54b957b227a1d34c2c6415ce860abd9c5a72d
Sujal Thomas Tatipelli <42236789+sujalthomas@users.noreply.github.com>
2022-10-26 03:17:25 -0700

    Add files via upload

---
+++ src/pages/Ecommerce.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { BsCurrencyDollar } from 'react-icons/bs';
+import { GoPrimitiveDot } from 'react-icons/go';
+import { IoIosMore } from 'react-icons/io';
+import { DropDownListComponent } from '@syncfusion/ej2-react-dropdowns';
+
+import { Stacked, Pie, Button, LineChart, SparkLine } from '../components';
+import { useStateContext } from '../contexts/ContextProvider';
+
+const DropDown = ({ currentMode }) => (
+  <div className="w-28 border-1 border-color px-2 py-1 rounded-md">
+    <DropDownListComponent id="time" fields={{ text: 'Time', value: 'Id' }} style={{ border: 'none', color: (currentMode === 'Dark') && 'white' }} value="1" dataSource={dropdownData} popupHeight="220px" popupWidth="120px" />
+  </div>
+);
+
+const Ecommerce = () => {
+  const { currentColor, currentMode } = useStateContext();
+
+  return (
+    <div className="mt-24">
+      
+
+    </div>
+  );
+};
+
+export default Ecommerce;
